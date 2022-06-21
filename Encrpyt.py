1ea93069ffc6a5bb287e56f3708b30157c0efc76
Raphael Bialystok-Joly <raphaelbialystok-joly@Raphaels-MacBook-Air.local>
2022-06-21 08:31:08 +0200

    done ecrypt

--- Encrpyt.py
+++ Encrpyt.py
@@ -1,3 +1,29 @@
 import os
-from cryptography.Fernet import Fernet
+from cryptography.fernet import Fernet
 
+files = []
+
+folder = input("enter path for your folder here")
+
+for file in os.listdir("/Users/raphaelbialystok-joly/Documents/Code/Python/Testencrypt"):
+    if file == "Decrypt.py" or file == " Encrypt.py" or file == "README.md" or file == "cryptkey.key":
+        continue
+    if os.path.isfile(file):
+        files.appened(file)
+
+print (files) 
+"""        
+print (files)
+
+key = Fernet.generate_key()
+
+with open("cryptkey.key", "wb") as cryptkey:
+    cryptkey.write(key)
+    
+for file in files:
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+    contents_encrypted = Fernet(key).encrypt(contents)
+    with open(file, "wb") as thefile:
+        thefile.write(contents_encrypted)
+"""

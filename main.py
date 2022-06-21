770746148885b2ff405231e7078ad685a97b9134
FlMonkey <75172047+FlMonkey@users.noreply.github.com>
2022-06-21 13:27:19 +0200

    check prior

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ def dore():
     x = input("D/E")
     x = x.upper
    
-    if x == E:
+    if x == "E":
         exec(open("Encrypt.py").read())
-    if x == D:
+    if x == "D":
         exec(open("Encrypt.py").read())
     else:
         print("unvalid input try again")

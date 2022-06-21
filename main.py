075eb0c6156428b4eb92a44ffc7c4105021fc57e
FlMonkey <75172047+FlMonkey@users.noreply.github.com>
2022-06-21 18:59:39 +0200

    main improvments

--- main.py
+++ main.py
@@ -1,13 +1,29 @@
-print("Type a d in order to Decrypt, Type e in order to encrypt")
+x = input("Folder Path Here")
+
+with open("usrinput.txt", "w") as f:
+    f.write(x)
+
+
+print("Type 1 in order to Encrypt, 2 in order to Decrypt, 3 to change Path, 4 in order to close")
+
 
 def dore():
-    x = input("D/E")
-    x = x.upper
+    print("1 to set/change Path (you need to do this first), 2 in order to Decrypt, Type 3 in order to Encrypt, 4 in order to close and clear saved path")
+    x = input("1/2/3/4 \n")
    
-    if x == "E":
+    if x == "2":
         exec(open("Encrypt.py").read())
-    if x == "D":
-        exec(open("Encrypt.py").read())
+        dore()
+    if x == "3":
+        exec(open("Decrypt.py").read())
+        dore()
+    if x == "1 ":
+        y = input("Enter Path here")
+        dore()
+    if x == "4":
+        with open("usrinput.txt", "w") as q:
+            q.write(x)
     else:
         print("unvalid input try again")
         dore()
+

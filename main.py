d3bd2691cb3dcd156a1c747b0dea74531222c031
FlMonkey <75172047+FlMonkey@users.noreply.github.com>
2022-06-21 19:21:11 +0200

    bug

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 x = input("Folder Path Here \n")
 
-with open("usrinput.txt", "r") as f:
+filez = open("usrinput.txt","w")
+filez.truncate(0)
+filez.close()
+
+with open("usrinput.txt", "w") as f:
     f.write(x)
 
 
@@ -9,7 +13,7 @@ print("Type 1 in order to Encrypt, 2 in order to Decrypt, 3 to change Path, 4 in
 
 
 def dore():
-    print("1 to set/change Path (you need to do this first), 2 in order to Decrypt, Type 3 in order to Encrypt, 4 in order to close and clear saved path")
+    print("1 to set/change Path (you need to do this first), 2 in order to Encrypt, Type 3 in order to Decrypt, 4 in order to close and clear saved path")
     x = input("1/2/3/4 \n")
    
     if x == "2":
@@ -18,14 +22,20 @@ def dore():
     if x == "3":
         exec(open("Decrypt.py").read())
         dore()
-    if x == "1 ":
+    if x == "1":
+        file = open("usrinput.txt","r+")
+        file.truncate(0)
+        file.close()
         y = input("Enter Path here")
         dore()
     if x == "4":
-        with open("usrinput.txt", "w") as q:
-            q.write(x)
+        filez = open("usrinput.txt","w")
+        filez.truncate(0)
+        filez.close()
+        exit()
     else:
         print("unvalid input try again")
         dore()
 
 dore()
+

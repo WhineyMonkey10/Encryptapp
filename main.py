fd55ad56ca755c459b17aca73a1dd8ec3189a4eb
FlMonkey <75172047+FlMonkey@users.noreply.github.com>
2022-06-21 13:26:43 +0200

    Main file
    Add Path option from begning as well as password option

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-exec(open("Encrypt.py").read())
+print("Type a d in order to Decrypt, Type e in order to encrypt")
+
+def dore():
+    x = input("D/E")
+    x = x.upper
+   
+    if x == E:
+        exec(open("Encrypt.py").read())
+    if x == D:
+        exec(open("Encrypt.py").read())
+    else:
+        print("unvalid input try again")
+        dore()
